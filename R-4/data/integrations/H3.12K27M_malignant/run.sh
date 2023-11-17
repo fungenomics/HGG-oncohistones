0eddaac7402a831bf223b4fc185ae4c97cccd100
sjessa <selinjessa@gmail.com>
2023-11-17 13:26:03 -0500

    Add configs for integration

---
+++ R-4/data/integrations/H3.12K27M_malignant/run.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/bash
+#SBATCH --job-name="harmony"
+#SBATCH --time=03:00:00
+#SBATCH --output=logs/integrate_harmony-%j.out
+#SBATCH --account=rrg-kleinman
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=50G
+
+mkdir -p logs/
+
+module load r/4.0.0
+
+# use the project specific library
+export R_LIBS_USER="../../../renv/library/R-4.0/x86_64-pc-linux-gnu"
+
+# Rscript ../../../../code/scripts/integrate_harmony.R
+Rscript ../../../../code/scripts/update_integration_metadata.R
